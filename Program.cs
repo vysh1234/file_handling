7913859733ca4e4ad5d655c49897da8823d7f5f1
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 10:46:22 +0530

    file handling in c#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace file_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see file handling:");
+
+            FileStream fs = new FileStream("C:\\Users\\vaishnavi.vinjam\\source\\repos\\file_handling\\file.txt",FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("let's read and display from file");
+
+            string str = sr.ReadLine();
+            while(str!=null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+        }
+    }
+}
